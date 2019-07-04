67492787ad3e0a1808307072a7dbd286b48f8990
EchoAndSophie <51267855+EchoAndSophie@users.noreply.github.com>
2019-07-04 11:09:24 +0800

    Update BootController.java

--- finaltestdemo/src/main/java/com/example/demo/controller/BootController.java
+++ finaltestdemo/src/main/java/com/example/demo/controller/BootController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import java.util.Map;
-
+//aaaaaa
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
